07c62c32528283eddab4d4f30d1aea819b9aeb86
Chase Hooper <43388614+HoopCha@users.noreply.github.com>
2019-10-16 09:53:48 -0600

    mvp done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+
+import Form from "./components/Form";
+import FormStructure from "./components/FormStructure";
 
 function App() {
+  const [members, setMember] = useState([
+    {
+      name: "",
+      email: "",
+      role: ""
+    }
+  ]);
+
+  const addNewMember = member => {
+    const newMember = {
+      name: member.name,
+      email: member.email,
+      role: member.role
+    };
+    setMember([...members, newMember]);
+  };
+
   return (
     <div className="App">
-
+      <h1>My Team Members</h1>
+      {/* we are going to pass a function down as a prop */}
+      <FormStructure addNewMember={addNewMember} />
+      <Form members={members} />
     </div>
   );
 }
